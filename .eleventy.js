fc2004b396b52d7c010e813c2b7f94b9eb399855
oumar-julien gaudens <oumar-julien.gaudens@i-carre.net>
2026-01-28 12:41:59 +0100

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,15 @@
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("src/assets"); // CSS, JS, images
+
+const isProd = process.env.NODE_ENV === "production";
+
+module.exports = function (eleventyConfig) {
+  // Copie des assets (CSS, JS, images)
+  eleventyConfig.addPassthroughCopy("src/assets");
 
   return {
+    pathPrefix: isProd ? "/oj.gaudens/" : "/",
     dir: {
       input: "src",          // dossier source
-      includes: "_includes", // dossiers pour layouts
+      includes: "_includes", // layouts
       output: "_site"
     }
   };
